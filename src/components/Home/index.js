1406e4caa79f7af2ca3a4256f98b356bee9c3a82
Saso Matejina <saso@checkr.com>
2017-08-10 09:54:14 -0700

    css modules

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,9 +3,11 @@ import { observer, inject } from 'mobx-react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
+import styles from './home.css';
+
 @inject("store") @observer
 @graphql(gql`
-  query allProjects {
+  query {
     projects {
       id
       name
@@ -24,7 +26,7 @@ export default class Home extends React.Component {
       return (
         <ul>
           {projects.map(project =>
-          <li key={project.id}>
+          <li key={project.id} className={styles.li}>
             {project.name} {" "}
             <span>{project.slug}</span>
           </li>
