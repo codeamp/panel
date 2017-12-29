12e7c04356f0e2ea0c997e007757a8544e325f41
Saso Matejina <matejina@gmail.com>
2017-12-28 22:57:22 -0800

    update styles

--- src/index.js
+++ src/index.js
@@ -18,6 +18,14 @@ import makeClient from 'lib/apollo';
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 import createPalette from 'material-ui/styles/createPalette';
 import { blue, pink } from 'material-ui/colors';
+import JssProvider from 'react-jss/lib/JssProvider';
+import { create } from 'jss';
+import preset from 'jss-preset-default';
+import { createGenerateClassName } from 'material-ui/styles';
+
+const generateClassName = createGenerateClassName();
+const jss = create(preset());
+jss.options.insertionPoint = 'insertion-point-jss';
 
 const client = makeClient('http://localhost:3011/query');
 const theme = createMuiTheme({
@@ -29,16 +37,18 @@ const theme = createMuiTheme({
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <Router>
-      <Provider store={store}>
-        <MuiThemeProvider theme={theme}>
-          <Switch>
-            <Route path='/login' component={Login} />
-            <Route path='/' component={App} />
-          </Switch>
-        </MuiThemeProvider>
-      </Provider>
-    </Router>
-  </ApolloProvider>, document.getElementById('root')
+  <JssProvider jss={jss} generateClassName={generateClassName}>
+    <ApolloProvider client={client}>
+      <Router>
+        <Provider store={store}>
+          <MuiThemeProvider theme={theme}>
+            <Switch>
+              <Route path='/login' component={Login} />
+              <Route path='/' component={App} />
+            </Switch>
+          </MuiThemeProvider>
+        </Provider>
+      </Router>
+    </ApolloProvider>
+  </JssProvider>, document.getElementById('root')
 )
