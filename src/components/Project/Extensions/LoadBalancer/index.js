5de03b8acfbee29157d453f58bc007722aba3c20
Saso Matejina <matejina@gmail.com>
2018-04-23 21:05:02 -0700

    checkpoint

--- src/components/Project/Extensions/LoadBalancer/index.js
+++ src/components/Project/Extensions/LoadBalancer/index.js
@@ -64,8 +64,8 @@ export default class LoadBalancer extends React.Component {
 
   componentWillMount(){
     const fields = [
-        'service',
         'name',
+        'service',
         'type',
         'listener_pairs',
         'listener_pairs[].port',
@@ -74,8 +74,8 @@ export default class LoadBalancer extends React.Component {
     ]
     const rules = {}
     const labels = {
+        'name': 'NAME',
         'service': 'SERVICE',
-        'name': 'SUBDOMAIN',
         'type': 'ACCESS',
         'listener_pairs': 'LISTENER PAIRS',
         'listener_pairs[].port': 'PORT',
@@ -163,12 +163,12 @@ export default class LoadBalancer extends React.Component {
       <div>
         <form onSubmit={(e) => e.preventDefault()}>
           <Grid container spacing={24}>
-            <Grid item xs={6}>
+            <Grid item xs={12}>
+              <InputField fullWidth={true} field={this.form.$('name')} />
+            </Grid>
+            <Grid item xs={12}>
               <SelectField fullWidth={true} field={this.form.$('service')} extraKey={'service'} />
             </Grid>
-            <Grid item xs={6}>
-              <InputField fullWidth={true} field={this.form.$('name')} />
-            </Grid>        
             <Grid item xs={12}>
               <SelectField fullWidth={true} field={this.form.$('type')} />
             </Grid>        
@@ -197,7 +197,7 @@ export default class LoadBalancer extends React.Component {
             )
             })}
             <Grid item xs={12}>
-              <Button raised type="secondary" onClick={this.form.$('listener_pairs').onAdd}>
+              <Button variant="raised" type="secondary" onClick={this.form.$('listener_pairs').onAdd}>
                 Add Port Map
               </Button>
             </Grid>        
