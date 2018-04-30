49d0225e4dde83ff81a4531dcbfd961fb3591391
Shreyas Jaganmohan <shreyas@checkr.com>
2018-04-30 11:04:08 -0700

    Fix click away for project extensions page

--- src/components/Project/Extensions/index.js
+++ src/components/Project/Extensions/index.js
@@ -312,7 +312,7 @@ export default class ProjectExtensions extends React.Component {
           classes={{
             paper: styles.drawer
           }}
-          onClose={() => {this.setState({ drawerOpen: false })}}          
+          onClose={() => {this.setState({ extensionDrawer: false })}}
           open={this.state.extensionDrawer.open}
         >
           {this.renderExtensionsDrawer()}
