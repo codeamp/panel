914e5fbcb98f31b45a3dfd0370a0b4a63152fac6
Shreyas Jaganmohan <shreyas@checkr.com>
2018-05-08 22:35:19 -0700

    quick commit

--- src/components/Admin/Secrets/index.js
+++ src/components/Admin/Secrets/index.js
@@ -274,7 +274,7 @@ export default class Secrets extends React.Component {
     this.form.$('key').set('disabled', false)
     this.form.$('isSecret').set('disabled', false)
     this.form.$('scope').set('disabled', false)
-    this.form.$('environment').set('disabled', false)
+    this.form.$('environmentID').set('disabled', false)
   
     this.openDrawer()
   }
