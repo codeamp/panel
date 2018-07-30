756a044d6ef14ef6537efa9805d5cc9b2726b728
Shreyas <shreyas.jaganmohan@gmail.com>
2018-07-30 16:06:48 -0700

    Add socket handlers

--- src/components/Project/Features/index.js
+++ src/components/Project/Features/index.js
@@ -209,7 +209,7 @@ export default class Features extends React.Component {
   syncGitCommits() {
     console.log('syncGitCommits')
     const { project } = this.props.data;
-    
+
     this.props.getGitCommits({
       variables: {
         projectID: project.id, 
@@ -241,8 +241,9 @@ export default class Features extends React.Component {
 
   setupSocketHandlers(){
     const { socket, match } = this.props;
-
+    console.log('socket handler', match.url.substring(1, match.url.length) + '/features')
     socket.on(match.url.substring(1, match.url.length) + '/features', (data) => {
+      console.log('new feature!')
       this.props.data.refetch()
     });    
   }
