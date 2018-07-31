0f04193408b40ee7baa07e4fc4515b1b1a41f8ba
Shreyas <shreyas.jaganmohan@gmail.com>
2018-07-31 16:36:05 -0700

    fix getReadableDuration style

--- src/components/Project/Releases/index.js
+++ src/components/Project/Releases/index.js
@@ -88,9 +88,15 @@ class ReleaseView extends React.Component {
     var minutes = Math.floor((seconds - (hours * 3600)) / 60);
     var seconds = seconds - (hours * 3600) - (minutes * 60);
 
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
-    if (seconds < 10) {seconds = "0"+seconds;}
+    if (hours < 10) {
+      hours = "0" + hours
+    }
+    if (minutes < 10) {
+      minutes = "0" + minutes
+    }
+    if (seconds < 10) {
+      seconds = "0" + seconds
+    }
 
     return hours+':'+minutes+':'+seconds;
   }
