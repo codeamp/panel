1ac381cc6f4cd87f74ba5418353236f7c45ba1c4
Andrew Ballman <35912177+aballman@users.noreply.github.com>
2018-11-16 13:18:03 -0800

    Add autocomplete prop to InputField. Use it in Route53 to disable autocomplete (#281)

--- src/components/Form/input-field.js
+++ src/components/Form/input-field.js
@@ -6,11 +6,11 @@ import InputLabel from 'material-ui/Input/InputLabel';
 import FormControl from 'material-ui/Form/FormControl';
 import FormHelperText from 'material-ui/Form/FormHelperText';
 
-export default observer(({field, fullWidth, disabled}) => (
+export default observer(({field, fullWidth, disabled, autoComplete}) => (
   <div className={styles.root}>
     <FormControl fullWidth={fullWidth} {...field.error ? {error: true} : {}}>
       <InputLabel htmlFor={field.id}>{field.label}</InputLabel>
-      <Input type={field.type} {...field.bind()} disabled={disabled}/>
+      <Input type={field.type} {...field.bind()} disabled={disabled} autoComplete={autoComplete}/>
       <FormHelperText>{field.error}</FormHelperText>
     </FormControl>
   </div>
