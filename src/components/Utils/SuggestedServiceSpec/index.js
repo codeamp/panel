ed016095d9597aac570ac68eca9a1bd6e0762b6d
Shreyas <shreyas.jaganmohan@gmail.com>
2018-12-17 17:04:44 -0800

    make view more concise

--- src/components/Utils/SuggestedServiceSpec/index.js
+++ src/components/Utils/SuggestedServiceSpec/index.js
@@ -8,32 +8,33 @@ import styles from './style.module.css';
 export default class SuggestedServiceSpec extends React.Component {
     render() {
       return(
-        <Card style={{ padding: 25 }}>
-          <Typography variant="subheading" style={{ fontSize: 20, marginBottom: 15 }}>Suggested</Typography>
-          <Grid container spacing={24} className={styles.grid}>
-            <Grid item xs={12}>
-              <Typography variant="subheading">CPU</Typography>
-              <Typography variant="body2">(millicpus)</Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <TextField value={this.props.suggestedServiceSpec.cpuRequest} label={"Request"} disabled/>
-            </Grid>
-            <Grid item xs={6}>
-              <TextField value={this.props.suggestedServiceSpec.cpuLimit} label={"Limit"} disabled />
-            </Grid>
-            <Grid item xs={12}>
-              <Typography variant="subheading">Memory</Typography>
-              <Typography variant="body2">(mb)</Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <TextField value={this.props.suggestedServiceSpec.memoryRequest} label={"Request"} disabled/>
-            </Grid>
-            <Grid item xs={6}>
-              <TextField value={this.props.suggestedServiceSpec.memoryLimit} label={"Limit"} disabled />
-            </Grid>                      
+      <Card style={{ padding: 20 }}>
+        <Grid container spacing={24} className={styles.grid}>
+          <Grid item xs={12}>
+            <Typography variant="subheading" style={{ fontSize: 20 }}>Suggested</Typography>                    
           </Grid>
-          <br/>
-        </Card>
+          <Grid item xs={12}>
+            <Typography variant="subheading">CPU</Typography>
+            <Typography variant="body2">(millicpus)</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <TextField value={this.props.suggestedServiceSpec.cpuRequest} label={"Request"} disabled fullWidth={true} />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField value={this.props.suggestedServiceSpec.cpuLimit} label={"Limit"} disabled fullWidth={true} />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="subheading">Memory</Typography>
+            <Typography variant="body2">(mb)</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <TextField value={this.props.suggestedServiceSpec.memoryRequest} label={"Request"} disabled fullWidth={true} />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField value={this.props.suggestedServiceSpec.memoryLimit} label={"Limit"} disabled fullWidth={true} />
+          </Grid>                      
+        </Grid>
+      </Card>
       ) 
     }
   }
